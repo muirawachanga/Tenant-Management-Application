fda4a127abc72f46ff439680b1ebbc7c30f2fa43
Stephen Wachanga <wachangasteve@gmail.com>
2022-10-23 18:16:42 +0000

    Add billing period tabulation.
    Allow creation of property and property units.
    
    Add invoicing into the tenancy contract.
    
    add utility services on the invoicing.
    
    Convert the contract to submittable documents.

--- tenantmanagementsystem/tenant_management_system/doctype/billing_period/billing_period.py
+++ tenantmanagementsystem/tenant_management_system/doctype/billing_period/billing_period.py
@@ -4,9 +4,6 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
-from datetime import date
-from frappe.utils import get_first_day, get_last_day, formatdate
-
 
 class BillingPeriod(Document):
     pass
--- tenantmanagementsystem/tenant_management_system/doctype/utility_item/utility_item.py
+++ tenantmanagementsystem/tenant_management_system/doctype/utility_item/utility_item.py
@@ -10,3 +10,28 @@ from frappe.model.document import Document
 
 class UtilityItem(Document):
     pass
+
+
+@frappe.whitelist()
+def utility_item_query(doctype, txt, searchfield, start, page_len, filters):
+    return frappe.db.sql(
+        (
+            "select ui.name from `tabUtility Item` as ui,\n"
+            "     `tabTenancy Contract Item` as tci\n"
+            "		where ui.{key} like %(txt)s\n"
+            "			{mcond}\n"
+            "     and tci.utility_item = ui.name and tci.parent = %(tc)s\n"
+            "		order by\n"
+            "			if(locate(%(_txt)s, ui.name), locate(%(_txt)s, ui.name), 99999),\n"
+            "			ui.idx desc,\n"
+            "			ui.name\n"
+            "		limit %(start)s, %(page_len)s"
+        ).format(**{"key": searchfield, "mcond": get_match_cond(doctype)}),
+        {
+            "txt": "%%%s%%" % txt,
+            "_txt": txt.replace("%", ""),
+            "start": start,
+            "page_len": page_len,
+            "tc": filters["tenancy_contract"],
+        },
+    )
